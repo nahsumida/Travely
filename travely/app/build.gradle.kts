ba7089eaadd3893a0bcb97f0839eb76d31f2d3b0
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-10-13 16:15:35 -0300

    feat: adicionando funcionalidade de longin - sem conexão com firebase ainda

--- travely/app/build.gradle.kts
+++ travely/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,7 +46,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.0")
+
 }
--- travely/app/src/main/java/com/isabellatressino/travely/LoginActivity.kt
+++ travely/app/src/main/java/com/isabellatressino/travely/LoginActivity.kt
@@ -1,20 +1,94 @@
 package com.isabellatressino.travely
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.isabellatressino.travely.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        with(binding) {
+            buttonContinue.setOnClickListener {
+                validUser(editUser.text.toString().trim(), editPassword.text.toString())
+            }
         }
+
+    }
+
+    private fun validUser(email: String, password: String) {
+
+        if (email.isBlank() or password.isBlank()) {
+            Toast.makeText(this, "Preencha os campos para continuar", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { authResult ->
+                if (authResult.user?.isEmailVerified == false) {
+                    auth.signOut()
+                    Toast.makeText(
+                        this,
+                        "Por favor, ative a conta através do link enviado no email e tente novamente!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return@addOnSuccessListener
+                }
+
+                FirebaseFirestore.getInstance()
+                    .collection("users")
+                    .document(authResult.user?.uid ?: "")
+                    .get()
+                    .addOnSuccessListener { snapshot ->
+                        if (snapshot.exists()) {
+                            Toast.makeText(this, "Login feito com sucesso", Toast.LENGTH_LONG)
+                                .show()
+                            // Aqui você pode redirecionar para a próxima tela
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Usuário não encontrado na base de dados.",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    .addOnFailureListener { error ->
+                        Log.e("Isa firebase firestore tag", error.message.toString())
+                        Log.d(
+                            "Isa firebase firestore tag",
+                            "UID do usuário: ${authResult.user?.uid}"
+                        )
+                        Toast.makeText(
+                            this,
+                            "Erro ao acessar os dados do usuário.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+            }.addOnFailureListener { exception ->
+                if (exception.message.toString() == "The email address is badly formatted.") {
+                    Toast.makeText(
+                        this,
+                        "Endereço de email inválido, por favor digite novamente!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Erro: Usuário e/ou senha inválidos.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 }
