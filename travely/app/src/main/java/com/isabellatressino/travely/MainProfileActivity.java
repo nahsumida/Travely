5e8935675aaaf218ef2e3930680a6e2ff4eb403d
Izabelle Oliveira <118742135+IzabelleOliveira@users.noreply.github.com>
2024-10-25 18:59:47 -0300

    update

--- travely/app/src/main/java/com/isabellatressino/travely/MainProfileActivity.java
+++
@@ -1,6 +0,0 @@
-package com.isabellatressino.travely;
-
-import android.app.Activity;
-
-public class MainProfileActivity extends Activity {
-}
--- travely/app/src/main/java/com/isabellatressino/travely/MainProfileActivity.kt
+++ travely/app/src/main/java/com/isabellatressino/travely/MainProfileActivity.kt
@@ -1,10 +1,12 @@
 package com.isabellatressino.travely
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 
 class MainProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,25 @@ class MainProfileActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvNome = findViewById<TextView>(R.id.tv_nome)
+        val tvNomeCompleto = findViewById<TextView>(R.id.tv_nome_completo)
+        val tvEmail = findViewById<TextView>(R.id.tv_email)
+        val tvTipoPerfil = findViewById<TextView>(R.id.tv_tipo_perfil)
+
+
+        carregarDadosUsuario(tvNome, tvNomeCompleto, tvEmail, tvTipoPerfil)
     }
+
+    private fun carregarDadosUsuario(
+        tvNome: TextView,
+        tvNomeCompleto: TextView,
+        tvEmail: TextView,
+        tvTipoPerfil: TextView
+    ) {
+
+        
+
+    }
+
 }
