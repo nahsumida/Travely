ccc0fd87b1952019b8b364b4e6cf59f21507a16f
Izabelle Oliveira <118742135+IzabelleOliveira@users.noreply.github.com>
2024-11-04 19:25:45 -0300

    update

---
+++ travely/app/src/main/java/com/isabellatressino/travely/activity/ConfirmActivity.kt
@@ -0,0 +1,27 @@
+package com.isabellatressino.travely.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.isabellatressino.travely.R
+import com.isabellatressino.travely.databinding.ActivityConfirmBinding
+import com.isabellatressino.travely.databinding.ActivityMainScreenBinding
+
+class ConfirmActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityConfirmBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfirmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMenu.setOnClickListener {
+            val intent = Intent(this, MainScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
